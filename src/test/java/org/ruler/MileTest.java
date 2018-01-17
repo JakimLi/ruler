59f1f299c7bb7ab2c6353494e021139a47f198da
jakimli <ljking7002@gmail.com>
2018-01-17 19:25:30 +0800

    null not equal to three mile

--- src/test/java/org/ruler/MileTest.java
+++ src/test/java/org/ruler/MileTest.java
@@ -22,4 +22,9 @@ public class MileTest {
     public void three_mile_not_equal_to_null() throws Exception {
         assertNotEquals(new Mile(3), null);
     }
+
+    @Test
+    public void null_not_equal_to_three_mile() throws Exception {
+        assertNotEquals(null, new Mile(3));
+    }
 }
--- tasking.md
+++ tasking.md
@@ -1,5 +1,5 @@
 3 Mile != null done
-null != 3 Mile
+null != 3 Mile done
 3 Mile == 3 Mile done
 3 Mile != 2 Mile done
-3 Mile != 4 Mile
+3 Mile != 4 Mile done
