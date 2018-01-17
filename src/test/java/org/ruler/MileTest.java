5c6e309e5e4539f46e182e6a2f0b3253df451019
jakimli <ljking7002@gmail.com>
2018-01-17 19:15:07 +0800

    inital commit, have a place to start with the test case

---
+++ src/test/java/org/ruler/MileTest.java
@@ -0,0 +1,13 @@
+package org.ruler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MileTest {
+
+    @Test
+    public void first_test() throws Exception {
+        assertTrue(true);
+    }
+}
